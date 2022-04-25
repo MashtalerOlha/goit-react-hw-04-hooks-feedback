44ed6f4ad66a821cdc395f561ff76a398d69d000
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-04-25 16:40:26 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Statistics } from './Statistic/Statistic';
+import { FeedbackOptions } from './Feedback/Feedback';
+import { Notification } from './Notifcation/Notification';
+import { Container } from './Container/Container';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = e => {
+    const target = e.currentTarget.name;
+
+    this.setState(prevState => ({
+      [target]: prevState[target] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const result = this.countTotalFeedback();
+    const { good } = this.state;
+    const percentage = (good * 100) / result;
+    return Math.round(percentage);
+  };
+
+  render() {
+    const totalFeedback = this.countTotalFeedback();
+    const options = Object.keys(this.state);
+    const entriesOptions = Object.entries(this.state);
+
+    return (
+      <Container>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.leaveFeedback}
+        />
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            options={entriesOptions}
+            total={totalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Container>
+    );
+  }
+}
